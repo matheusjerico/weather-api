d125b373fd9fc36dd41db73998d551918e8c5ab0
Matheus Jericó Palhares <matheusjerico1994@hotmail.com>
2023-03-28 12:22:39 -0300

    feat: adding events in home page

--- views/home.py
+++ views/home.py
@@ -2,17 +2,19 @@ from fastapi import APIRouter
 from fastapi.responses import RedirectResponse
 from starlette.templating import Jinja2Templates
 from starlette.requests import Request
-
+from services import report_service
 
 templates = Jinja2Templates(directory="templates")
 router = APIRouter()
 
 
-@router.get("/")
-def index(request: Request):
-    return templates.TemplateResponse("home/index.html", {"request": request})
+@router.get("/", include_in_schema=False)
+async def index(request: Request):
+    events = await report_service.get_reports()
+    data = {"request": request, "events": events}
+    return templates.TemplateResponse("home/index.html", data)
 
 
-@router.get("/favicon.ico")
+@router.get("/favicon.ico", include_in_schema=False)
 def favicon():
     return RedirectResponse(url="/static/img/favicon.ico")
